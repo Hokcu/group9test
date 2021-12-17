8717171b553c54e978ee10b4e98904b0127bdfac
Hokcu <tosimasu19@gmail.com>
2021-12-17 15:36:00 +0100

    HO

---
+++ day2_printing_comments/task2_gasReceipt.java
@@ -0,0 +1,21 @@
+package day2_printing_comments;
+
+public class task2_gasReceipt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("+------------------------+");
+		System.out.println("I                        I");
+		System.out.println("I     MCLEAN STORE       I");
+		System.out.println("I                        I");
+		System.out.println("I 2019-06-19  04:38PM    I");
+		System.out.println("I                        I");
+		System.out.println("I Gallons:       10.870  I");
+		System.out.println("I Price/gallon: $ 2.089  I");
+		System.out.println("I                        I");
+		System.out.println("I Fuel total:  $ 22.71   I");
+		System.out.println("I                        I");
+		System.out.println("+------------------------+");
+	}
+
+}
---
+++ day2_printing_comments/task3_printExercise.java
@@ -0,0 +1,27 @@
+package day2_printing_comments;
+
+public class task3_printExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mike");
+		System.out.println("Smith");
+		System.out.println("England");
+		System.out.println("Germany");
+		System.out.println();
+		
+		System.out.print("Hello");
+		System.out.print("All");
+		System.out.println("Friends");
+		System.out.println("Hotel");
+		System.out.println("McLean");
+		System.out.println();
+		
+		System.out.println("this is EU5");
+		
+		System.out.println();
+		System.out.println("I am from " + "Turkey");
+		
+	}
+
+}
